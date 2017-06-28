e012c5b8e30a9004e04b2c01f14914301e0bfb5c
sagid <sagidana1@gmail.com>
2017-06-28 18:32:31 +0300

    edit main to use C: instead

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import video_splitter
 
-video_path = 'D:/data/hackathon/'
+video_path = 'C:/data/hackathon/'
 video_name = '1.avi'
 video_full_name = os.path.join(video_path, video_name)
 output_path = os.path.join(video_path,os.path.splitext(video_name)[0])
