59741b96c74d66f2e9d693a6e6683fa8c4670a34
yonatan <yonatansss36@gmail.com>
2017-06-29 06:13:14 +0300

    added tom's features

--- feature_extraction.py
+++ feature_extraction.py
@@ -8,13 +8,16 @@ from features_extractors import *
 def create_csv(videos_path, output_file, timestamps):
     out_file = open(output_file, "wb")
     writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    writer.writerow(['video_id', 'section_id', 'timestamp', 'movement'])
+    features = ['video_id', 'section_id', 'timestamp', 'movement',"movement_avarage","movement_var", \
+    "movement_std","num_of_elements_avarage","num_of_elements_var","num_of_elements_std", \
+    "num_of_contours_avarage","num_of_contours_var","num_of_contours_std","max_contour_size", \
+    "bg_change_above_15000","bg_change_above_45000","bg_change_above_75000","bg_change_above_105000", \
+    "bg_change_above_135000","max_bg_diff","max_bg_diff_avarage","max_bg_diff_var","max_bg_diff_std"]
+    writer.writerow(features)
+    out_file.close()
 
     for directory in os.listdir(videos_path):
         dir_path = os.path.join(videos_path, directory)
-
-        out_file = open(output_file, "ab")
-        writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
         if os.path.isdir(dir_path):
             for video in os.listdir(dir_path):
@@ -25,20 +28,25 @@ def create_csv(videos_path, output_file, timestamps):
 
                 date = parser.parse(timestamps[video_id])
                 date += datetime.timedelta(seconds=int(offset))
-                row.append(str(date))
+                row.append(date.hour)
 
                 features = __extract(os.path.join(dir_path, video))
                 row.extend(features)
 
+                out_file = open(output_file, "ab")
+                writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
                 writer.writerow(row)
-        out_file.close()
+                out_file.close()
 
 def __extract(video_full_path):
     features_list = []
 
     motion = MotionDetectionExtractor()
+    tom = TOM_FEATURES()
 
     features_list.extend(motion.extract(video_full_path))
+    features_list.extend(tom.extract(video_full_path))
 
     return features_list
  
